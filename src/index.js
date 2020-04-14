ecdaa7678797eb58990703563a093e531452809a
chrisuping <1756988314@qq.com>
2020-04-14 23:16:24 +0800

    Refactor structure

--- src/index.js
+++ src/index.js
@@ -1,48 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { ApolloProvider } from "react-apollo";
-import { ApolloClient } from "apollo-client";
-import { ApolloLink } from "apollo-link";
-import { HttpLink } from "apollo-link-http";
-import { onError } from "apollo-link-error";
-import { InMemoryCache } from "apollo-cache-inmemory";
-
 import * as serviceWorker from "./serviceWorker";
-import App from "./App";
+import App from "./app/index";
 
-// import "./style.css";
-
-const GITHUB_BASE_URL = "https://api.github.com/graphql";
-
-const httpLink = new HttpLink({
-  uri: GITHUB_BASE_URL,
-  headers: {
-    authorization: `Bearer ${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}`,
-  },
-});
-console.log(process.env);
-const errorLink = onError(({ graphQLErrors, networkError }) => {
-  if (graphQLErrors) {
-    graphQLErrors.map(({ message, locations, path }) =>
-      console.log(
-        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
-      )
-    );
-  }
-
-  if (networkError) {
-    console.log(`[Network error]: ${networkError}`);
-  }
-});
-
-const link = ApolloLink.from([errorLink, httpLink]);
-
-const cache = new InMemoryCache();
-
-const client = new ApolloClient({
-  link,
-  cache,
-});
+import client from "./actions/client";
 
 ReactDOM.render(
   <ApolloProvider client={client}>
